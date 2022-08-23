f89d6922cf463832c01c08050814ecf6b68471de
Peter Castle <vagnerpetel2020@gmail.com>
2022-08-23 10:59:10 -0300

    first commit

---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,21 @@
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
+
+  const logout = () => {
+    // remove user from localStorage.
+    localStorage.removeItem("user");
+
+    // dispatch logout action
+    dispatch({ type: "LOGOUT" });
+    workoutsDispatch({
+      type: "SET_WORKOUTS",
+      payload: null,
+    });
+  };
+
+  return { logout };
+};
